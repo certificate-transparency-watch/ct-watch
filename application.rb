f9679256a0f1c9ed5952ba4999bc7a16cf9c5e09
Tom Fitzhenry <tom@tom-fitzhenry.me.uk>
2014-01-12 10:32:44 +0000

    Enable CORS

--- application.rb
+++ application.rb
@@ -5,6 +5,8 @@ class CtWatch < Sinatra::Base
     helpers Sinatra::JSON
 
     get '/logserver/:id' do
+        cross_origin :allow_origin => 'http://ct-watch.tom-fitzhenry.me.uk'
+
         conn = PG.connect :hostaddr => '172.17.42.1', :user => 'docker', :password => 'docker', :dbname => 'ct-watch'
         results = conn.exec_params("SELECT sth.* FROM sth JOIN log_server ON sth.log_server_id = log_server.id WHERE log_server.name = $1", [params[:id]])
         good, bad = results.partition { |row| row.values_at('verified') }
