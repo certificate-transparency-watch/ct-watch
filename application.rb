e10c9657449b59f12c2a0d3c34b9345829e6a821
Tom Fitzhenry <tom@tom-fitzhenry.me.uk>
2014-01-12 18:37:43 +0000

    Typefail: Partition good/bad STH correctly
    I should really just be using ActiveRecord...

--- application.rb
+++ application.rb
@@ -11,7 +11,7 @@ class CtWatch < Sinatra::Base
 
         conn = PG.connect :hostaddr => '172.17.42.1', :user => 'docker', :password => 'docker', :dbname => 'ct-watch'
         results = conn.exec_params("SELECT sth.* FROM sth JOIN log_server ON sth.log_server_id = log_server.id WHERE log_server.name = $1", [params[:id]])
-        good, bad = results.partition { |row| row.values_at('verified') }
+        good, bad = results.partition { |row| row.values_at('verified')[0] == 't' }
         json :good => good, :bad => bad
     end
 end
