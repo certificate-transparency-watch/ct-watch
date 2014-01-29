4380a6b86879bd4f646a7b5912943b017214e9a6
Tom Fitzhenry <tom@tom-fitzhenry.me.uk>
2014-01-28 20:17:29 -0500

    Only allow searching of domains
    NOT tlds (e.g. ".com")
    NOT suffix of domains (e.g. "oogle.com")
    
    YES "google.com" -> ["mail.google.com", "talk.google.com", "google.com"]

--- application.rb
+++ application.rb
@@ -1,6 +1,7 @@
 require 'sinatra'
 require 'sinatra/json'
 require 'sinatra/cross_origin'
+require 'public_suffix'
 
 class CtWatch < Sinatra::Base
     helpers Sinatra::JSON
@@ -16,8 +17,9 @@ class CtWatch < Sinatra::Base
     end
 
     get '/domain/:domain' do
+	halt 404 if not PublicSuffix.valid?(params[:domain])
         conn = PG.connect :hostaddr => '172.17.42.1', :user => 'docker', :password => 'docker', :dbname => 'ct-watch'
-        results = conn.exec_params("SELECT log_server_id, domain, encode(leaf_input, 'base64'), encode(extra_data, 'base64') FROM log_entry WHERE reverse(domain) like ($1 || '%')", [params[:domain].reverse])
+        results = conn.exec_params("SELECT log_server_id, domain, encode(leaf_input, 'base64'), encode(extra_data, 'base64') FROM log_entry WHERE domain = $1 OR reverse(domain) like ($1 || '.%')", [params[:domain].reverse])
         json :results => results.values
     end
 end
